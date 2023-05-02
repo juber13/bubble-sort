7b77dce01c9331d05d4f389705d6b1bd7ae637c4
juber <juberk1010@gmail.com>
2023-05-02 12:52:25 +0530

    make insertion sort

--- list.java
+++ list.java
@@ -17,6 +17,7 @@ class Main{
    // reverse a array
    int arr[] = {2,4,1,6,5};
 
+    // bubble sort
     for(int i = 0; i < arr.length - 1; i++){
         for(int j = 0; j < arr.length - i - 1; j++){
             if(arr[j] > arr[j + 1]){
@@ -27,7 +28,7 @@ class Main{
         }
     }
 
-
+   // selection sort
     for(int i = 0; i < arr.length -1; i++){
         int smallest = i;
         for(int j = i + 1; j < arr.length; j++){
@@ -40,6 +41,25 @@ class Main{
         arr[smallest] = arr[i];
         arr[i] = temp;
     }
+
+    // insertion sort
+
+    for(int i = 1; i < arr.length; i++){
+        int curr = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && curr < arr[j]){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        // placement
+
+        arr[j + 1] = curr;
+    }
+     
+
+
 
     System.out.print(Arrays.toString(arr));
 
